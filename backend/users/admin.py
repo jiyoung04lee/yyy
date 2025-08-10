fe95445edb1f79a45680bbdb2dcce673d5478a72
xodls <pider6@naver.com>
2025-08-10 23:04:27 +0900

    [BE] FEAT: 유저의 admins.py 설정

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # 리스트/폼에서 보일 필드를 약간 손보면 좋음
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("nickname",)}),
+    )
+    
