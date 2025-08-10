5c694065e2bc9ee4ed9b7ec52082da0caf94565d
xodls <pider6@naver.com>
2025-08-10 23:09:02 +0900

    [BE] FEAT: 카카오 로그인과 유저 연결  테이블 모델 구현

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class User(AbstractUser):
      nickname = models.CharField(max_length=30, blank=True)
-     
+
+class SocialAccount(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    provider = models.CharField(max_length=20, default="kakao")
+    social_id = models.CharField(max_length=255, unique=True, db_index=True)
+
+    class Meta:
+        unique_together = ("provider", "social_id")
