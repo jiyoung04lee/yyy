6d2360fc83b2bd00613e99fd30f8bcd4e587c049
xodls <pider6@naver.com>
2025-08-10 23:09:51 +0900

    [BE] FEAT: 커스텀 유저 settings.py에 등록

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -146,6 +146,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = "users.User"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
