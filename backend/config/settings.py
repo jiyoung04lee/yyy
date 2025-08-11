4bb2e8a932ac5a2fed441656bf6080959f96dee4
jenajeong <skg4660@gmail.com>
2025-08-11 15:08:23 +0900

    [BE]FEAT: 커스텀 유저 모델 등록(config/settings.py)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User" # 커스텀 유저 모델 생성
