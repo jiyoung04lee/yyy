bf7f410245c6f3f593b4f60bfec97e66ab805d57
jiyoung <jenn4843@naver.com>
2025-08-13 01:20:36 +0900

    [FE]FEAT: 웹페이지 휴대폰 사이즈 app.jsx에 실행 코드 구현

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AppFrame from './components/AppFrame.jsx';
+
+export default function App() {
+  return (
+    <AppFrame>
+      {/* 여기에 네 UI 컴포넌트들 넣기 */}
+      <div>
+      </div>
+    </AppFrame>
+  );
+}
+
+
+
+
+
+
