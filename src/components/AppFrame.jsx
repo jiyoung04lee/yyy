7952fd8117c4439e8b7f1c642dc2416d0cdce26e
jiyoung <jenn4843@naver.com>
2025-08-13 05:30:27 +0900

    [FE]FIX: UI에 맞게 화면 비율 수정 후 주석 제거

--- src/components/AppFrame.jsx
+++ src/components/AppFrame.jsx
@@ -1,17 +1,3 @@
-// import './appframe.css';
-
-// export default function AppFrame({ children }) {
-//   return (
-//     <div className="viewport-lock">
-//       <div className="phone">
-//         <div className="app-scroll">
-//           {children}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import './appframe.css';
 
 export default function AppFrame({ children }) {
