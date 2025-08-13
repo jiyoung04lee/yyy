0e27cc176bc677cdfa8f5c22ae2df1748adb9577
jiwon-02 <fyfy4987@naver.com>
2025-08-13 11:52:01 +0900

    [FE]FEAT: 상단 바 구현1 (Header.jsx 추가)

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <button className="bell-button" aria-label="알림"></button>
+    </header>
+  );
+}
