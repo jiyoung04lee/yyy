f2b38ee838cbf69d0e69c907d28376cc6f0e9864
jiwon-02 <fyfy4987@naver.com>
2025-08-15 07:02:24 +0900

    [FE]FEAT: 상단 바 알림 버튼 누르면 알림창 페이지로  이동하도록 구현

--- frontend/src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.css';
-
-export default function Header() {
-  return (
-    <header className="header">
-      <button className="bell-button" aria-label="알림"></button>
-    </header>
-  );
-}
