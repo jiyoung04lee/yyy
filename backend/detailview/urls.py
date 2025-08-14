5b6a815645e3bbbef1c9ecbae7e653b8b078579f
xodls <pider6@naver.com>
2025-08-15 03:58:46 +0900

    [BE]FEAT : AI전용 파티 생성 api 설정 (url연결)

--- backend/detailview/urls.py
+++ backend/detailview/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PartyViewSet, PartyJoinAPIView, PartyLeaveAPIView
+from .views import PartyViewSet, PartyJoinAPIView, PartyLeaveAPIView,AIPartyCreateAPIView
 
 app_name = "detailview"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("parties/<int:party_id>/join/", PartyJoinAPIView.as_view(), name="party-join"),
     path("parties/<int:party_id>/leave/", PartyLeaveAPIView.as_view(), name="party-leave"),  # 선택
+    path("party/ai-create/", AIPartyCreateAPIView.as_view(), name="ai-party-create"),
 ]
