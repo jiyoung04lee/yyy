32450096b28cf7bff0810fd97f45ead4c03dbe21
jenajeong <skg4660@gmail.com>
2025-08-14 12:53:47 +0900

    [BE]FEAT: Review 모델 구축 (mypage/models.py)

--- backend/mypage/models.py
+++ backend/mypage/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from django.conf import settings
 
+class Review(models.Model):
+    party = models.ForeignKey('detailview.Party', on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    q1_rating = models.PositiveSmallIntegerField()
+    q2_rating = models.PositiveSmallIntegerField()
+    q3_rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('party', 'user')  # 한 유저당 한 파티 리뷰 1개만
+
+    def __str__(self):
+        return f"{self.user.username} - {self.party.title} 리뷰"
