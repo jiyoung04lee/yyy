9b8aca768b1e8686a80c18576b1b6544f6371015
xodls <pider6@naver.com>
2025-08-15 22:56:10 +0900

    [BE]FIX: GPT API 연결확인및 수정 && test코드 추가

--- backend/utils/partyAI.py
+++ backend/utils/partyAI.py
@@ -1,7 +1,9 @@
-import openai
+from openai import OpenAI
 from django.conf import settings
+import json
 
-openai.api_key = settings.OPENAI_API_KEY
+# openai.api_key = settings.OPENAI_API_KEY 구버전 설정 방식
+client = OpenAI(api_key=settings.OPENAI_API_KEY) # 최신 버전 설정 방식
 
 def generate_party_by_ai(place_name: str) -> dict:
     prompt = f"""
@@ -15,19 +17,15 @@ def generate_party_by_ai(place_name: str) -> dict:
         "tags": ["음악", "친목"]
     }}
     """
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0.7,
-        response_format={"type": "json_object"}, # JSON 응답 강제
-    )
-
-    content = response.choices[0].message.content.strip()
-    
-    import json
     try:
-        data = json.loads(content)
-        return data
-    except Exception:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.7,
+
+        )
+
+        return json.loads(response.choices[0].message.content.strip())
+    except Exception as e:
+        print(f"[AI 생성 실패]: {e}")
         return {}  # 실패시 빈 dict 반환
