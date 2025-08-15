52bf72321cd2606b2fb75c5b05dd3e3ab8da1091
jiyoung <jenn4843@naver.com>
2025-08-16 01:39:57 +0900

    [FE]FIX: Header 추가 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// scrollpage.jsx
 import AppFrame from './components/AppFrame.jsx';
 import PhoneSheet from './scrollPage/Scroll_sheet.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
     <AppFrame>
       <div className="phone-stage">
-        {/* 여기 배경(지도나 이미지) 넣어도 됨 */}
+        <Header/>
         <PhoneSheet />
       </div>
     </AppFrame>
