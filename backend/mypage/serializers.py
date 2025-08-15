15257d106796d7d321bea746fcf3a03d088347d0
jenajeong <skg4660@gmail.com>
2025-08-15 19:32:14 +0900

    [BE]FEAT: 신고 시리얼라이저 구현(mypage/serializers.py)

--- backend/mypage/serializers.py
+++ backend/mypage/serializers.py
@@ -17,3 +17,27 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
+    
+    
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = ['id', 'party', 'reporter', 'reported_user', 'category', 'content', 'status', 'created_at']
+        read_only_fields = ['id', 'reporter', 'status', 'created_at']
+
+    def validate(self, data):
+        reporter = self.context['request'].user
+        reported_user = data.get('reported_user')
+        party = data.get('party')
+
+        if reporter == reported_user:
+            raise serializers.ValidationError("자기 자신은 신고할 수 없습니다.")
+
+        if Report.objects.filter(party=party, reporter=reporter, reported_user=reported_user).exists():
+            raise serializers.ValidationError("이미 해당 유저를 신고하셨습니다.")
+
+        return data
+
+    def create(self, validated_data):
+        validated_data['reporter'] = self.context['request'].user
+        return super().create(validated_data)
