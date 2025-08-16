018b84ac9d8434228bf7604e07b68d2bc8f4b8fa
jenajeong <skg4660@gmail.com>
2025-08-17 03:29:17 +0900

    [BE]FIX: 필요없는 import삭제

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -12,7 +12,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import partyassist.routing
-import notice.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
