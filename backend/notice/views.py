392ce43cd4447cc1a187cb3a50dcac6cc9391cb1
jenajeong <skg4660@gmail.com>
2025-08-17 02:23:44 +0900

    [BE]FEAT: 알림 뷰 설정 (notice/views.py)

--- backend/notice/views.py
+++ backend/notice/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Notice
+from .serializers import NoticeSerializer
 
-# Create your views here.
+class NoticeViewSet(viewsets.ModelViewSet):
+    serializer_class = NoticeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notice.objects.filter(user=self.request.user)
+
+    # 읽음 처리 (PATCH)
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_read = True
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
