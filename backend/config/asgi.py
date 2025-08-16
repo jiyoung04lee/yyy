df8091c47b55448964ec1d49a1c83e884989cc8d
jenajeong <skg4660@gmail.com>
2025-08-17 01:07:43 +0900

    [BE]FIX: WebSocket 같은 실시간 연결을 처리할 수 있도록 asgi 설정 (confing/asgi.py)

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import partyassist.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            partyassist.routing.websocket_urlpatterns
+        )
+    ),
+})
