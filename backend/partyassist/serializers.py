1e53ba567098c59137ffa31d874c23b78f3fbe9f
jenajeong <skg4660@gmail.com>
2025-08-17 01:48:58 +0900

    [BE] FEAT: 파티 참석자 리스트 조회 시리얼라이저 구현 (partyassist/serializers.py)

--- backend/partyassist/serializers.py
+++ backend/partyassist/serializers.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
-from detailview.models import Party
+from detailview.models import Party, Participation
+from mypage.models import ExtraSetting
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+class ExtraSettingSerializer(serializers.ModelSerializer):
+    mbti = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ExtraSetting
+        fields = ["grade", "college", "personality", "mbti"]
+
+    def get_mbti(self, obj):
+        # 네 필드 합쳐서 "ENFP" 같은 문자열로 반환
+        return f"{obj.mbti_i_e}{obj.mbti_n_s}{obj.mbti_f_t}{obj.mbti_p_j}".upper()
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    extra_setting = ExtraSettingSerializer(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "profile_image",
+            "intro",
+            "extra_setting"  # 추가 설정 정보 포함
+        ]
+
+class PartyParticipantSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer(read_only=True)
+
+    class Meta:
+        model = Participation
+        fields = ["id", "user", "is_standby"]
+        
+        
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
