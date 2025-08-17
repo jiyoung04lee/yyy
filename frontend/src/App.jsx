563e4e3906b67f2509c014a17dd65d8360ede536
xodls <pider6@naver.com>
2025-08-18 00:55:15 +0900

    [FE]FIX: src폴더 이동 및 의존성 체크 및 주석처리를 통한 테스트 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,28 @@
 import AppFrame from './components/AppFrame.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Notifications from "./pages/Notifications.jsx";
+// import Home from "./pages/Home.jsx";
+// import Notifications from "./pages/Notifications.jsx";
 import Map from "./pages/Map.jsx";
-import Assist from "./pages/Assist.jsx";
+// import Assist from "./pages/Assist.jsx";
 
-import Mypage from "./pages/Mypage.jsx";
-import PartyHistory from './pages/PartyHistory.jsx';   
+// import Mypage from "./pages/Mypage.jsx";
+// import PartyHistory from './pages/PartyHistory.jsx';   
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <AppFrame>
           <Routes>
-            <Route path="/mypage" element={<Mypage />} />
+            {/* <Route path="/mypage" element={<Mypage />} />
             <Route path="/mypage/history" element={<PartyHistory />} />
             <Route index element={<Home />} />
-            <Route path="/notifications" element={<Notifications />} /> 
+            <Route path="/notifications" element={<Notifications />} />  */}
             <Route path="/map" element={<Map />} />
-            <Route path="/assist" element={<Assist />} />
+            {/* <Route path="/assist" element={<Assist />} />
             <Route path="/mypage" element={<Mypage />} />
-            <Route path="/mypage/history" element={<PartyHistory />} /> 
+            <Route path="/mypage/history" element={<PartyHistory />} />  */}
           </Routes>
       </AppFrame>
-    </Router>
+    </BrowserRouter>
   );
 }
