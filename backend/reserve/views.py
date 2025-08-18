3abb5e0791a55400731b848d594a3b2c7e3d88ec
jenajeong <skg4660@gmail.com>
2025-08-19 04:35:35 +0900

    [BE] 예약과 상세보기 분리 및 예약 모델에 결제방식 카테고리 (detailview, reserve)

--- backend/reserve/views.py
+++ backend/reserve/views.py
@@ -3,11 +3,6 @@ from rest_framework.response import Response
 from .serializers import JoinAndReserveSerializer, PaymentSerializer
 
 class JoinAndReserveView(generics.CreateAPIView):
-    """
-    참가 신청 + 결제를 동시에 처리하는 API
-    POST /api/reserve/join/
-    Body: { "party_id": 1 }
-    """
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = JoinAndReserveSerializer
 
@@ -15,7 +10,10 @@ class JoinAndReserveView(generics.CreateAPIView):
         return {"request": self.request, **super().get_serializer_context()}
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        # URL 경로에서 party_id 꺼내기
+        party_id = self.kwargs.get("party_id")
+        serializer = self.get_serializer(data={"party_id": party_id})
         serializer.is_valid(raise_exception=True)
         payment = serializer.save()
         return Response(PaymentSerializer(payment).data)
+    
