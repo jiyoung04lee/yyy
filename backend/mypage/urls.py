0064a08154ee3baea048c186fdefc048638f2fd6
jenajeong <skg4660@gmail.com>
2025-08-18 17:21:36 +0900

    [BE]FIX: 마이페이지 url 용도에 맞게 수정 (mypage/urls.py)

--- backend/mypage/urls.py
+++ backend/mypage/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 from .views import MypageMainViewSet, ReviewViewSet, ReportViewSet, ExtraSettingViewSet, ProfileUpdateView
 
 router = DefaultRouter()
-router.register(r'mypage', MypageMainViewSet, basename='mypage')  # 메인 화면
+router.register(r'main', MypageMainViewSet, basename='main')  # 메인 화면
 router.register(r'reviews', ReviewViewSet, basename='review')
 router.register(r'reports', ReportViewSet, basename='report')
 router.register(r'extra-settings', ExtraSettingViewSet, basename='extra-setting')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('mypage/profile/', ProfileUpdateView.as_view(), name='mypage-profile-update'),
+    path('profile/', ProfileUpdateView.as_view(), name='mypage-profile-update'),
 ]
