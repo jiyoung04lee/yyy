7cfdaa18286191eceea67d36f327110b7262dbe8
jiyoung <jenn4843@naver.com>
2025-08-23 06:43:42 +0900

    4

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,8 +82,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://3.34.135.176",
     "https://partyboom.online",#구매한 도메인
     "https://www.partyboom.online", 
-    # "https://yyy-khaki.vercel"
-    "https://yyy-f2c7wa4ob-jiyoung04lees-projects.vercel.app"
+    "https://yyy-ad0xw5rj9-jiyoung04lees-projects.vercel.app/"
 ]
 
 SIMPLE_JWT = {
